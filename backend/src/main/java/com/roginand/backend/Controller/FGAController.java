ca4d1dc19d09a3c3394dccfa6cfc0167afbd4df9
Ginanddd <137371866+Ginanddd@users.noreply.github.com>
2025-06-02 13:12:29 +0800

    changed the url of the frontend

--- backend/src/main/java/com/roginand/backend/Controller/FGAController.java
+++ backend/src/main/java/com/roginand/backend/Controller/FGAController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin("https://fair-group-assignment.vercel.app")
 public class FGAController {
     private final FGAService fgaService;
     
