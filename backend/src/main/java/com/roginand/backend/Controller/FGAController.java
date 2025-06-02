98dcb32aaa33c4ad288f4bb9540734c1974b2b9c
Ginanddd <137371866+Ginanddd@users.noreply.github.com>
2025-06-02 12:41:01 +0800

    tryfix cors

--- backend/src/main/java/com/roginand/backend/Controller/FGAController.java
+++ backend/src/main/java/com/roginand/backend/Controller/FGAController.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +26,6 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "https://fair-group-assignment-r91ubkthj-roginandds-projects.vercel.app")
 public class FGAController {
     private final FGAService fgaService;
     
